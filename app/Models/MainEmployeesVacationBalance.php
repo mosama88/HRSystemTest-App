96c43f11d00d5624d894dd13300302d69413f7db
mosama88 <mosama88@hotmail>
2024-10-05 15:11:06 +0300

    Done18

--- app/Models/MainEmployeesVacationBalance.php
+++ app/Models/MainEmployeesVacationBalance.php
@@ -5,12 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class MainEmployeesVacation extends Model
+class MainEmployeesVacationBalance extends Model
 {
     use HasFactory;
-
-
-    protected $table = "main_employees_vacations";
+    protected $table = "main_employees_vacation_balances";
 
     protected $guarded = [];
 
--- database/migrations/2024_10_05_151008_create_main_employees_vacation_balances_table.php
+++ database/migrations/2024_10_05_151008_create_main_employees_vacation_balances_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('main_employees_vacations', function (Blueprint $table) {
+        Schema::create('main_employees_vacation_balances', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('main_employees_vacations');
+        Schema::dropIfExists('main_employees_vacation_balances');
     }
 };
