ca0875d32c07fcd787cb578649b0693b61300443
mosama88 <mosama88@hotmail.com>
2024-10-10 09:36:05 +0300

    Done

--- resources/views/dashboard/vacationsBalance/index.blade.php
+++ resources/views/dashboard/vacationsBalance/index.blade.php
@@ -258,7 +258,7 @@
                                 </tbody>
                             </table>
                             <div class="col-md-12 text-center" id="ajax-pagination_in_search">
-                                {{ $data->links('pagination::bootstrap-5') }}
+                                {{-- {{ $data->links('pagination::bootstrap-5') }} --}}
                             </div>
                         @else
                             <div class="alert alert-warning" role="alert" dir="rtl">
