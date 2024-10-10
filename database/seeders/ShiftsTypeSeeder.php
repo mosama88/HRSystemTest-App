a4e5e26f4efd18b3ef4b43d6df0968c566d4f46a
mosama88 <mosama88@hotmail.com>
2024-10-10 10:39:53 +0300

    Don3

--- database/seeders/ShiftsTypeSeeder.php
+++ database/seeders/ShiftsTypeSeeder.php
@@ -13,62 +13,74 @@ class ShiftsTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        
+
         DB::table('shifts_types')->delete();
         DB::table('shifts_types')->insert([
             [
                 'id' => 1,
-                'type' => 3,
-                'from_time' => '00:00:00',
-                'to_time' => '12:00:00',
+                'employee_id' => 3,
+                'start_time' => '00:00:00',
+                'end_time' => '12:00:00',
                 'total_hours' => '8.00',
-                'active' => 1,
-                'created_by' => 1,
-                'updated_by' => 1,
-                'com_code' => 1,
+                'is_active' => 1,
+                'branch_id' => 1,
+                'department_id' => 1,
+                'job_id' => 1,
                 'created_at' => null,
-                'updated_at' => '2024-09-07 08:47:47',
+                'updated_at' => '2024-09-07 05:47:47'
             ],
             [
                 'id' => 2,
-                'type' => 1,
-                'from_time' => '11:00:00',
-                'to_time' => '19:00:00',
+                'employee_id' => 1,
+                'start_time' => '11:00:00',
+                'end_time' => '19:00:00',
                 'total_hours' => '8.00',
-                'active' => 1,
-                'created_by' => 1,
-                'updated_by' => null,
-                'com_code' => 1,
-                'created_at' => '2024-09-07 08:41:50',
-                'updated_at' => '2024-09-07 08:45:00',
+                'is_active' => 1,
+                'branch_id' => 1,
+                'department_id' => null,
+                'job_id' => 1,
+                'created_at' => '2024-09-07 05:41:50',
+                'updated_at' => '2024-09-07 05:45:00'
             ],
             [
                 'id' => 3,
-                'type' => 1,
-                'from_time' => '08:30:00',
-                'to_time' => '15:00:00',
+                'employee_id' => 1,
+                'start_time' => '08:30:00',
+                'end_time' => '15:00:00',
                 'total_hours' => '8.00',
-                'active' => 1,
-                'created_by' => 1,
-                'updated_by' => 1,
-                'com_code' => 1,
-                'created_at' => '2024-09-07 08:42:23',
-                'updated_at' => '2024-09-07 08:47:14',
+                'is_active' => 1,
+                'branch_id' => 1,
+                'department_id' => 1,
+                'job_id' => 1,
+                'created_at' => '2024-09-07 05:42:23',
+                'updated_at' => '2024-09-07 05:47:14'
             ],
             [
                 'id' => 4,
-                'type' => 2,
-                'from_time' => '13:00:00',
-                'to_time' => '21:00:00',
+                'employee_id' => 2,
+                'start_time' => '13:00:00',
+                'end_time' => '21:00:00',
                 'total_hours' => '8.00',
-                'active' => 1,
-                'created_by' => 1,
-                'updated_by' => null,
-                'com_code' => 1,
-                'created_at' => '2024-09-07 08:48:16',
-                'updated_at' => '2024-09-07 08:48:16',
-            ]
+                'is_active' => 1,
+                'branch_id' => 1,
+                'department_id' => null,
+                'job_id' => 1,
+                'created_at' => '2024-09-07 05:48:16',
+                'updated_at' => '2024-09-07 05:48:16'
+            ],
+            [
+                'id' => 5,
+                'employee_id' => 1,
+                'start_time' => '00:00:00',
+                'end_time' => '08:00:00',
+                'total_hours' => '8.00',
+                'is_active' => 1,
+                'branch_id' => 1,
+                'department_id' => null,
+                'job_id' => 1,
+                'created_at' => '2024-10-10 07:37:36',
+                'updated_at' => '2024-10-10 07:37:36'
+            ],
         ]);
-
     }
 }
