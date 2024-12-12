bcbe78c537c7bd18d1d8891856e1ab6aad3b7228
mosama88 <mosama88@hotmail>
2024-12-12 18:14:04 +0200

    Done Go

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -14,6 +14,6 @@ class EmployeeSeeder extends Seeder
      */
     public function run(): void
     {
-        Employee::factory()->count(25)->create();
+        Employee::factory()->count(300)->create();
     }
 }
