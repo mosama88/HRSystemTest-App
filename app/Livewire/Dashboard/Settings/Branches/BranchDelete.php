273d0576d221c821c5d581c352bd0cb6f045ac47
Mohamed Osama <mosama88@hotmail.com>
2024-12-15 11:41:35 +0200

    Done Good4

--- app/Livewire/Dashboard/Settings/Branches/BranchDelete.php
+++ app/Livewire/Dashboard/Settings/Branches/BranchDelete.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Dashboard\Settings\Branches;
 
 use App\Models\Branch;
 use Livewire\Component;
+use App\Models\Employee;
 use App\Livewire\Dashboard\Settings\Branches\BranchTable;
 
 class BranchDelete extends Component
@@ -21,6 +22,16 @@ public $branchDeleteRecored,$name;
 
     public function submit(){
 
+        $com_code = auth()->user()->com_code;
+            $data = get_Columns_where_row(new Branch(), array('*'), array("id" => $this->branchDeleteRecored->id, "com_code" => $com_code));
+            if (empty($data)) {
+                return redirect()->route('dashboard.branches.index')->withErrors(['error' => 'عفوآ غير قادر على الوصول للبيانات المطلوبه']);
+            }
+            $counterUsed = get_count_where(new Employee(), array("com_code" => $com_code, "branch_id" => $this->branchDeleteRecored->id));
+            if ($counterUsed > 0) {
+                return redirect()->route('dashboard.branches.index')->with(['error' => 'عفوآ غير قادر على الحذف لانه قد تم أستخدامه من قبل']);
+            }
+            
                 // Save Data
     $this->branchDeleteRecored->delete();
     $this->reset('branchDeleteRecored');
