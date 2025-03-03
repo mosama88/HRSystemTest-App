c181e7568c4a921fdd7970b16174f2b347ca6f68
mosama88 <mosama88@hotmail>
2025-03-03 21:41:27 +0200

    Done Api

---
+++ app/Http/Controllers/Dashboard/Api/BranchController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BranchController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
